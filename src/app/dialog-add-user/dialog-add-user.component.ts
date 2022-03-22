474c87885260bb9d1fce93dba87a782e27a8ae8f
Gurpreet Singh <gurpreet.singh@gmx.de>
2022-03-22 17:14:08 +0100

    Second version

---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,36 @@
+import { Component, Injectable, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { User } from 'src/models/user.class';
+
+
+@Component({
+  selector: 'app-dialog-add-user',
+  templateUrl: './dialog-add-user.component.html',
+  styleUrls: ['./dialog-add-user.component.scss']
+})
+export class DialogAddUserComponent implements OnInit {
+
+  user = new User();
+  birthDate!: Date;
+
+  constructor(private firestore: AngularFirestore) { }
+
+  ngOnInit(): void {
+  }
+
+  saveUser(){
+    this.user.birthDate = this.birthDate.getTime();
+    console.log('current user is', this.user);
+    
+    this.firestore
+    .collection('users')
+    .add(this.user.toJSON())
+    .then((result: any ) =>{
+      console.log('adding user', result);  
+    });
+  }
+  
+
+
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'simple-crm-5da43',
+    appId: '1:184621031179:web:e0c372995d5c17a3e88c0e',
+    storageBucket: 'simple-crm-5da43.appspot.com',
+    apiKey: 'AIzaSyCOyD4aC5upSBFOzd648JM977Ir9ZhAXL4',
+    authDomain: 'simple-crm-5da43.firebaseapp.com',
+    messagingSenderId: '184621031179',
+  },
   production: false
 };
 
